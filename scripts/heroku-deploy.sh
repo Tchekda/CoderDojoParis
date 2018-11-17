f6ba89368a5ce74fb02d1988b10d8732d32e5b28
Tchekda <tchekda@gmail.com>
2018-11-17 15:10:34 +0100

    Heroku

--- scripts/heroku-deploy.sh
+++ scripts/heroku-deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-python manage.py collectstatic -l --noinput
+# python manage.py collectstatic -l --noinput
 python manage.py makemigrations Core Homepage Dashboard
 python manage.py migrate
 python manage.py test -v2
